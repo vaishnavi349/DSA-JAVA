46f0daa9c628fee704d78645331c9bd4a82f8a5c
vaishnavi349 <vankalasvaishnavi9@gmail.com>
2025-10-18 06:38:30 +0530

    Add files via upload

---
+++ arrayliststring.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+class arrayliststring{
+public static void main(String[] args){
+ArrayList<String> al = new ArrayList<String>();
+al.add("vaish");
+al.add("gauri");
+al.add("aditi");
+System.out.println(al);
+}
+}
