5a3595ffb05a7d55b8ccdcb3cb2979545152cbc4
vaishnavi349 <vankalasvaishnavi9@gmail.com>
2026-03-06 16:23:14 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String str="VAISHNAVI";
+		StringBuilder reversed=new StringBuilder(str).reverse();
+		System.out.println(reversed);
+	}
+
+	}
+
+
