b69af10f99c0ad6d39fa3e66fce06893063b95be
vaishnavi349 <vankalasvaishnavi9@gmail.com>
2026-03-07 17:09:37 +0530

    Add files via upload

---
+++ CountDigits.java
@@ -0,0 +1,11 @@
+package com.practice;
+
+public class CountDigits {
+
+	public static void main(String[] args) {
+		int num=1234567890;
+		int count=String.valueOf(num).length();
+		System.out.println(count);
+	}
+
+}
